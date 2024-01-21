d6c52e4fd4d73da924dcaa3cc053b24e966d9677
Abdur Rab Khan <abdurrabkhan3263@gmail.com>
2024-01-21 10:38:15 +0530

    Second Commit

--- script.js
+++ script.js
@@ -4,16 +4,16 @@ let difficut = 'easy';
 let quesNum = 10;
 
 let catObj = {
-    Category : 22,
-    Geography : 22,
-    History : 23,
-    Science : 17,
-    General : 9,
-    Computer : 18,
-    Maths : 19,
-    Arts : 25,
-    Animals : 27,
-    Sports : 21,
+    Category: 22,
+    Geography: 22,
+    History: 23,
+    Science: 17,
+    General: 9,
+    Computer: 18,
+    Maths: 19,
+    Arts: 25,
+    Animals: 27,
+    Sports: 21,
 }
 
 const cat = document.querySelector('#cat');
@@ -21,30 +21,82 @@ const diff = document.querySelector('#diffi');
 const queValue = document.querySelector('#num');
 const catOp = document.querySelectorAll('.cat-op')
 
-function changeBackground(value){
+function changeBackground(value) {
     category = catObj[value];
     const body = document.body;
     body.style.backgroundImage = `url(img/${value}.jpg)`;
 }
 
-cat.addEventListener('change' , (event=>{
+cat.addEventListener('change', (event => {
     changeBackground(cat.value);
 }));
 
-diff.addEventListener('change' , (event=>{
+diff.addEventListener('change', (event => {
     difficut = diff.value.toLowerCase();
 }));
 
-queValue.addEventListener('change' , (event=>{
+queValue.addEventListener('change', (event => {
     quesNum = queValue.value;
 }));
 
+// START SUBMIT BTN
 
 let startBtn = document.querySelector('#start-btn');
-startBtn.addEventListener('click' , ()=>{
-    console.log("category   ",category);
-    console.log("difficutltltrtf   ",difficut);
-    console.log("questionsfnyu    ",quesNum);
+let start = document.querySelector('.start');
+let questions = document.querySelector('.questions');
+
+let counter = 0;
+let arr;
+
+// QUESTIONS SELECTORS;
+const mainQues = document.querySelector('#main-question');
+const option = document.querySelector('.un-list')
+
+async function addingQestions() {
+    const url = (`https://opentdb.com/api.php?amount=${quesNum}&category=${category}&difficulty=${difficut}&type=multiple`);
+    const response = await fetch(url);
+    const result = await response.json();
+    arr = await result.results;
+}
+
+
+async function loadQues(){
+    let data = arr[counter];
+    let Ques = data['question'];
+    let options1 = data['incorrect_answers'][0];
+    let options2 = data['incorrect_answers'][1];
+    let options3 = data['incorrect_answers'][2];
+    let options4 = data['correct_answer'];
+    let correctAns = data['correct_answer'];
+    console.log(Ques)
+    console.log(options1)
+    console.log(options2)
+    console.log(options3)
+    console.log(options4)
+}
+
+startBtn.addEventListener('click', async () => {
+    if (start.style.display == 'flex') {
+        start.style.display = `none`;
+    }
+    if (questions.style.display == 'none') {
+        questions.style.display = 'block';
+    }
+    await addingQestions();
+    await loadQues();
+    counter++;
+})
+
+
+// ADDING QUESTION 
+let nextQues = document.querySelector('#ans-next');
+
+nextQues.addEventListener('click', (event) => {
+    if(counter >= arr.length){
+        return;
+    }
+    loadQues();
+    counter++;
 })
 
 
@@ -85,17 +137,3 @@ startBtn.addEventListener('click' , ()=>{
 
 // background-color: rgb(79, 111, 82); color: white;
 // background-color: rgb(234, 144, 108); color: white;
-
-
-// const apiCalling = async () =>{
-//     const url = (`https://opentdb.com/api.php?amount=11&category=23&difficulty=medium`);
-//     const response = await fetch(url);
-//     const result = await response.json();
-//     apiData(result);
-// }
-
-// function apiData(data){
-//     console.log(data);
-// }
-
-// apiCalling();
